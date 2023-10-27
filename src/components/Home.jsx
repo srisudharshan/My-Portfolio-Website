4779cc0f2e4f3183f3ada2bedfb7ff085f07d787
Sri Sudharshan <sri_sudharshan@yahoo.com>
2023-10-27 23:42:24 +0530

    Created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import heroImg from '../assets/images/heroImg.jpg';
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h2>
+                    I am a Full Stack Developer
+                </h2>
+                <p>
+
+                </p>
+            </div>
+             <button>
+                Portfolio <MdOutlineKeyboardArrowRight/>
+             </button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
