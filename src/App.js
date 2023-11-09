a79c37069509b8a6e903ed76dd8205e3ecb80225
Sri Sudharshan <sri_sudharshan@yahoo.com>
2023-11-09 18:31:10 +0530

    SocialLinks componenet completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <NavBar/>
       <Home/>
 
-
       <SocialLinks/>
     </div>
   );
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { FaGithub,FaLinkedin } from 'react-icons/fa'
+import {FaGithub,FaLinkedin } from 'react-icons/fa'
 import {HiOutlineMail} from "react-icons/hi"
-import {BsFillPersonalLinesFill} from "react-icons/bs"
+import {BsFillPersonLinesFill} from "react-icons/bs"
 
 const SocialLinks = () => {
 
@@ -18,7 +18,7 @@ const SocialLinks = () => {
         {
             id:2,
             child:(
-                <>GitHub <FaLinkedin size={30}/>
+                <>GitHub <FaGithub size={30}/>
                 </>
             ),
             href: 'https://github.com',
@@ -35,7 +35,7 @@ const SocialLinks = () => {
         {
             id:4,
             child:(
-                <>Resume <BsFillPersonalLinesFill size={30}/>
+                <>Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
             href: '/SriSudharshanResume.pdf',
@@ -45,17 +45,21 @@ const SocialLinks = () => {
     ]
 
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
-        {links.map(({id,child,href,download})=>{
-                <li key={id} className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] 
-                hover:ml-[10px] hover:rounded-md duration-300'>
-                 <a href="" className='flex justify-between items-center w-full text-white'>
-                    <>LinkedIn <FaLinkedin size={30}/>
-                    </>
+        {links.map(({id,child,href,download,style})=>(
+                <li key={id} 
+                className={"flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[10px] hover:rounded-md duration-300" 
+                + " " + 
+                style }>
+                 <a href={href} className='flex justify-between items-center w-full text-white'
+                 download={download} 
+                 target='_blank'
+                 rel="noreferrer">
+                    {child}
                  </a>
                 </li>
-            })}
+            ))}
             
         </ul>
     </div>
