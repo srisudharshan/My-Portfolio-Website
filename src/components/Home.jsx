2f3f58529a0471947d304d14bb17a81909926c90
Sri Sudharshan <sri_sudharshan@yahoo.com>
2023-11-11 06:53:43 +0530

    Contact component completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import heroImg from '../assets/images/heroImg.jpg';
 import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+import {Link} from 'react-scroll';
 
 const Home = () => {
   return (
@@ -19,10 +20,10 @@ const Home = () => {
                 </p>
             
             <div>
-             <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+             <Link to="portfolio" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                 Portfolio 
                 <span className='group-hover:rotate-90 duration-300'> <MdOutlineKeyboardArrowRight size={25} className='ml-1'/> </span>
-             </button>
+             </Link>
              </div>
              </div>
              <div>
